1859bd8e9cb3021647cc27dbc56a8b65e5da9d00
Carter Dooley <carter.j.dooley43@gmail.com>
2023-01-18 12:05:58 -0600

    Lab 1

---
+++ labs/lab1.py
@@ -0,0 +1,9 @@
+# Author: Carter Dooley
+# ULID/Section: C00419163 Section 001
+
+myName = input("Enter your name: ")
+print(myName)
+
+tempF = input("Enter a temperature in farenhiet: ")
+tempC = (tempF -32) * 5/9
+print("The temperature in celcius is:", tempC)
